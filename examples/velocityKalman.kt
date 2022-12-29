587f6c4f329fdb3599e973a244b64558180f9abd
kratman <kratmandu@gmail.com>
2022-12-29 17:15:28 -0500

    Making the time step a parameter to demonstrate how to use params

--- examples/velocityKalman.kt
+++ examples/velocityKalman.kt
@@ -5,6 +5,8 @@ import org.jetbrains.kotlinx.multik.api.*
 import org.jetbrains.kotlinx.multik.ndarray.data.*
 
 class VelocityFilter : UnscentedBase {
+    private val timeStepIndex = 1
+    
     constructor(x: Double, y: Double) : super(4, 2, 0.25) {
         setState(mk.ndarray(mk[x, y, 0.0, 0.0]))
         setVariance(mk.ndarray(mk[mk[5.0, 0.0, 0.0, 0.0],
@@ -20,9 +22,8 @@ class VelocityFilter : UnscentedBase {
 
     override fun predictModel(state: array1D, parameters: array1D) : array1D {
         val newState = state.copy()
-        val timeStep = 1.0
-        newState[0] += state[2] * timeStep
-        newState[1] += state[3] * timeStep
+        newState[0] += state[2] * parameters[timeStepIndex]
+        newState[1] += state[3] * parameters[timeStepIndex]
         return newState
     }
 
